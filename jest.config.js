3d66403795758042dcca407ccd11b38079ea5d16
Michael Nascimento <michael.nsc@outlook.com>
2021-09-08 02:11:09 -0300

    chore: add jest and babel config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,4 @@ module.exports = {
   collectCoverageFrom: ["<rootDir>/src/**/*.{ts,tsx}"],
   coverageDirectory: "coverage",
   testEnvironment: "node",
-  transform: {
-    ".+\\.ts$": "ts-jest",
-  },
 };
