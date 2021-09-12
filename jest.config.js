d871356fa50163a1ba1f7d38b9c48d0ccfd76dc9
Michael Nascimento <michael.nsc@outlook.com>
2021-09-12 16:53:31 -0300

    refactor: change imports to use modules

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,8 @@ module.exports = {
   transform: {
     ".+\\.ts$": "ts-jest",
   },
+  moduleNameMapper: {
+    "@domain/(.*)": "<rootDir>/src/domain/$1",
+    "@data/(.*)": "<rootDir>/src/data/$1",
+  },
 };
